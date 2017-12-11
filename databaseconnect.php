b57d9860f1ef584e0799c67f771bbf3f535d30f7
JenniferBenedict <jbenedict1@drew.edu>
2017-12-11 15:33:17 -0500

    changed db name

--- databaseconnect.php
+++ databaseconnect.php
@@ -3,8 +3,9 @@
 $user = 'ps93j2ru0f6x1av1';
 $password = 'qlwtb0988ju4tet5';
 $db = 'syts3ep6wfq9xjo0';
-$host = 'dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	';
+$host = 'dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $port = 3306;
+
 
 $link = mysqli_init();
 $conn = mysqli_real_connect(
